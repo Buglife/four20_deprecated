e79ee1630059831ad097b405e109ec82ed4faca4
Dave Schukin <daveschukin@gmail.com>
2021-03-20 00:11:02 -0700

    AVCaptureDevice. ft_frontFacingARKitCamera

--- Source/iOS/Extensions/AVFoundation/AVCaptureDevice+Additions.swift
+++ Source/iOS/Extensions/AVFoundation/AVCaptureDevice+Additions.swift
@@ -4,6 +4,7 @@
 //
 
 import AVFoundation
+import ARKit
 
 private let kFrontCameraAspectRatio: CGFloat = 4.0 / 3.0
 
@@ -14,6 +15,28 @@ public extension AVCaptureDevice {
     class func ft_availableCaptureDevices(mediaType: AVMediaType = .video, position: AVCaptureDevice.Position = .unspecified) -> [AVCaptureDevice] {
         let session = AVCaptureDevice.DiscoverySession(deviceTypes: .ft_all, mediaType: .video, position: position)
         return session.devices.sorted { $0.ft_deviceTypeAndPosition < $1.ft_deviceTypeAndPosition }
+    }
+    
+    /// Well this is fucked. You would think you could grab the dimensions directly from AVCaptureDevice.
+    /// Unfortunately AVCaptureDevice.activeFormat changes *after* an ARKit session starts, therefore
+    /// the dimensions it returns also change (tested on iPhone 11 Pro, iOS 13.3).
+    /// So we hardcode this for now.
+    ///
+    /// - Warning: Make sure you assert that this value is equal to the `ARFrame` size returned by ARKit!
+    static let ft_frontFacingTrueDepthCameraDimensions = CGSize(width: 1440.0, height: 1080.0)
+    
+    /// This generally returns the TrueDepth camera, except on iPhone SE 2 on iOS 14, which supports
+    /// `ARFaceTrackingConfiguration` with its non-TrueDepth camera.
+    class var ft_frontFacingARKitCamera: AVCaptureDevice? {
+        if let result = AVCaptureDevice.default(.builtInTrueDepthCamera, for: .video, position: .front) {
+            return result
+        }
+        
+        if ARFaceTrackingConfiguration.isSupported {
+            return AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+        }
+        
+        return nil
     }
     
     class var ft_frontFacingTrueDepthCamera: AVCaptureDevice? {
