35c996b3a80f3bb34c04b030c09b34e0dcced011
Dave Schukin <daveschukin@gmail.com>
2021-03-23 09:44:53 -0700

    Minor AVAudioSession debug improvements

--- Source/iOS/Extensions/AVFoundation/AVAudioSessionRouteDescription+Additions.swift
+++ Source/iOS/Extensions/AVFoundation/AVAudioSessionRouteDescription+Additions.swift
@@ -6,7 +6,7 @@ import AVFoundation
 
 public extension AVAudioSessionRouteDescription {
     var ft_debugDescription: String {
-        let inputStrs = inputs.map { $0.portName }.joined(separator: ", ")
+        let inputStrs = inputs.map(\.portName).joined(separator: ", ")
         let outputStr = outputs.map(\.portName).joined(separator: ", ")
         return "\(inputStrs) / \(outputStr)"
     }
--- Source/iOS/Extensions/AVFoundation/Notification+AVAudioSession.swift
+++ Source/iOS/Extensions/AVFoundation/Notification+AVAudioSession.swift
@@ -9,10 +9,12 @@ public extension Notification {
     /// - Returns: A debug description that assumes the notification is one of those related to `AVAudioSession`.
     /// - SeeAlso: [AVAudioSession](https://developer.apple.com/documentation/avfaudio/avaudiosession)
     var ft_audioSessionDebugDescription: String {
+        let readableName = readable(name.rawValue)
+        
         if let userInfoStr = ft_audioSessionUserInfoString {
-            return "\(name) | \(userInfoStr)"
+            return "\(readableName) | \(userInfoStr)"
         } else {
-            return "\(name)"
+            return readableName
         }
     }
     
@@ -36,7 +38,12 @@ public extension Notification {
             userInfoStrs[key.debugDescription] = "\(val)"
         }
         
-        let pairs: [String] = userInfoStrs.map { "\($0): \($1)" }
+        let pairs: [String] = userInfoStrs.keys.sorted().reversed().map { key in
+            let rawVal = userInfoStrs[key] ?? "nil"
+            let length = max(rawVal.count, 14)
+            let val = rawVal.padding(toLength: length, withPad: " ", startingAt: 0)
+            return "\(readable(key)): \(val)"
+        }
         guard !pairs.isEmpty else { return nil }
         return pairs.joined(separator: ", ")
     }
@@ -55,5 +62,26 @@ public extension Notification {
     
     var ft_audioSessionRouteChangePreviousRoute: AVAudioSessionRouteDescription? {
         userInfo?[AVAudioSessionRouteChangePreviousRouteKey] as? AVAudioSessionRouteDescription
+    }
+}
+
+fileprivate func readable(_ val: String) -> String {
+    switch val {
+    case AVAudioSession.interruptionNotification.rawValue:
+        return "interruptionNotification"
+    case AVAudioSession.routeChangeNotification.rawValue:
+        return "routeChangeNotification"
+    case AVAudioSession.mediaServicesWereResetNotification.rawValue:
+        return "mediaServicesWereResetNotification"
+    case AVAudioSession.mediaServicesWereLostNotification.rawValue:
+        return "mediaServicesWereLostNotification"
+    case AVAudioSessionRouteChangeReasonKey:
+        return "routeChangeReason"
+    case AVAudioSessionRouteChangePreviousRouteKey:
+        return "previousRoute"
+    case AVAudioSessionInterruptionTypeKey:
+        return "interruptionType"
+    default:
+        return val
     }
 }
