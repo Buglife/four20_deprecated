71d339bb54a48af1599e06c4250d8b9e47665a0c
Daniel DeCovnick <danhd123@mac.com>
2022-03-28 16:08:10 -0700

    Faster ft_eulerAngles implementation

--- Source/Shared/Extensions/simd/simd_float4x4+Additions.swift
+++ Source/Shared/Extensions/simd/simd_float4x4+Additions.swift
@@ -17,9 +17,43 @@ public extension simd_float4x4 {
     }
     
     var ft_eulerAngles: SCNVector3 {
-        let node = SCNNode()
-        node.simdTransform = self
-        return node.eulerAngles
+        // adapted from https://one-week-apps.com/arkit-transform-matrices-quaternions-and-related-conversions/
+        // intended to be equivalend to:
+        //let node = SCNNode()
+        //node.simdTransform = self
+        //return node.eulerAngles
+
+        
+        //first we get the quaternion from m00...m22
+        //see http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+        let qw = sqrt(1 + self.columns.0.x + self.columns.1.y + self.columns.2.z) / 2.0
+        let qx = -((self.columns.2.y - self.columns.1.z) / (qw * 4.0)) // divergence from source: multiply qx, qy, and qz by -1 to match SceneKit
+        let qy = -((self.columns.0.z - self.columns.2.x) / (qw * 4.0))
+        let qz = -((self.columns.1.x - self.columns.0.y) / (qw * 4.0))
+        
+        //then we deduce euler angles with some cosines
+        //see https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        // roll (x-axis rotation)
+        let sinr = +2.0 * (qw * qx + qy * qz)
+        let cosr = +1.0 - 2.0 * (qx * qx + qy * qy)
+        let roll = atan2(sinr, cosr)
+
+        // pitch (y-axis rotation)
+        let sinp = +2.0 * (qw * qy - qz * qx)
+        var pitch: Float
+        if fabs(sinp) >= 1 {
+             pitch = copysign(Float.pi / 2, sinp)
+        } else {
+            pitch = asin(sinp)
+        }
+
+        // yaw (z-axis rotation)
+        let siny = +2.0 * (qw * qz + qx * qy)
+        let cosy = +1.0 - 2.0 * (qy * qy + qz * qz)
+        let yaw = atan2(siny, cosy)
+        
+        let manualEulers = SCNVector3(roll, pitch, yaw) // correct for ARKit/scenekit conventions (vs SCNVector3(pitch, yaw, roll))
+        return manualEulers
     }
 }
 
